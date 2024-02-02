5897504d76510b65859b367460c05feb4f005c24
soumalya70 <soumalya70>
2024-02-02 23:29:42 +0530

    Home page created & maped views with Urls

--- core/base/views.py
+++ core/base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+import datetime
+import requests
 # Create your views here.
+def home(request):
+    current_time=datetime.datetime.now()
+    quote_url = "https://api.quotable.io/random"
+    quote_response = requests.get(quote_url)
+    quote_data = quote_response.json()
+    random_quote = quote_data.get("content")
+    author=quote_data.get("authorSlug")
+
+    context = {'random_quote': random_quote,'current_time': current_time, 'author': author}
+    return render(request, 'home.html',context)
